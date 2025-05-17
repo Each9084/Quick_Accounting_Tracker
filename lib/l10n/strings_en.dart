878b04151a5000e15c8ae61eed48532894045368
Each9084 <each9084@gmail.com>
2025-05-17 21:00:41 +0100

    international language

---
+++ lib/l10n/strings_en.dart
@@ -0,0 +1,10 @@
+final localizedEn = {
+  'confirm': 'Save',
+  'equal': '=',
+  'clear': 'Clear',
+  'delete': 'Delete',
+  'income': 'Income',
+  'expense': 'Expense',
+  'note_hint': 'Note (Optional)',
+  'date': 'Date',
+};
---
+++ lib/l10n/strings_zh.dart
@@ -0,0 +1,11 @@
+final localizedZh = {
+  'confirm': '保存',
+  'equal': '=',
+  'clear': '清空',
+  'delete': '删除',
+  'income': '收入',
+  'expense': '支出',
+  'note_hint': '请输入备注（可选）',
+  'date': '日期',
+  // 添加更多 key-value
+};
