56926453c8812887a7a8fdf9377e68d215773c57
Each9084 <each9084@gmail.com>
2025-05-22 00:00:09 +0100

    rename the parameter name

--- lib/data/dataModel/bill_entity.dart
+++ lib/data/dataModel/bill_entity.dart
@@ -1,22 +1,22 @@
 //数据库专用的数据模型, 本地数据模型（可与 UI 层解耦）
 class BillEntity {
   final int? id;
-  final String userId;
+  final int user_id;
   final double amount;
   final String note;
-  final String categoryName;
-  final String categoryIcon;
-  final bool isIncome;
+  final String category_name;
+  final int category_icon_code;
+  final bool is_income;
   final String date;
 
   BillEntity({ //未来是自增的,没必要required
     this.id,
-    required this.userId,
+    required this.user_id,
     required this.amount,
     required this.note,
-    required this.categoryName,
-    required this.categoryIcon,
-    required this.isIncome,
+    required this.category_name,
+    required this.category_icon_code,
+    required this.is_income,
     required this.date});
 
   //  将BillEntity 类的实例转换为一个 Map<String, dynamic> 类型的对象
@@ -24,12 +24,12 @@ class BillEntity {
   Map<String,dynamic> toMap(){
     return {
       "id":id,
-      "userId":userId,
+      "user_id":user_id,
       "amount":amount,
       "note":note,
-      "categoryName":categoryName,
-      "categoryIcon":categoryIcon,
-      "isIncome":isIncome?1:0,
+      "category_name":category_name,
+      "category_icon_code":category_icon_code,
+      "is_income":is_income?1:0,
       "date":date,
     };
   }
@@ -40,12 +40,12 @@ class BillEntity {
   //普通构造函数 如上面的BillEntity 就只能进行简单的赋值和初始化
   factory BillEntity.fromMap(Map<String, dynamic> map){
     return BillEntity(id: map["id"],
-        userId: map["userId"],
+        user_id: map["user_id"],
         amount: map["amount"],
         note: map["note"],
-        categoryName: map["categoryName"],
-        categoryIcon: map["categoryIcon"],
-        isIncome: map["isIncome"] == 1,
+        category_name: map["category_name"],
+        category_icon_code: map["category_icon_code"],
+        is_income: map["is_income"] == 1,
         date: map["date"]);
   }
 
