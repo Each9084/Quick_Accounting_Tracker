121d8999cc97f16610d8008667f3e74df0e94121
Each9084 <each9084@gmail.com>
2025-05-26 13:01:57 +0100

    Normalization

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,36 @@
-import 'dart:async';
-
-import 'package:accounting_tracker/data/dao/user_dao.dart';
-import 'package:accounting_tracker/data/dataModel/user_entity.dart';
-import 'package:accounting_tracker/data/db/app_database.dart';
-import 'package:accounting_tracker/l10n/strings.dart';
+import 'package:accounting_tracker/l10n/Strings.dart';
 import 'package:accounting_tracker/screens/bill_home_page.dart';
+import 'package:accounting_tracker/screens/category_manager_page.dart';
+import 'package:accounting_tracker/screens/language_setting_page.dart';
+import 'package:accounting_tracker/screens/setting_page.dart';
 import 'package:accounting_tracker/service/user_service.dart';
-import 'package:accounting_tracker/test/db_test_page.dart';
+import 'package:accounting_tracker/theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:month_year_picker/month_year_picker.dart';
+import 'package:provider/provider.dart';
+import 'l10n/language_notifier.dart';
+import 'theme/theme_notifier.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 国际化语言加载
-  Strings.load(Locale("zh"));
+  // 国际化语言,初始加载
+  StringsMain.load(Locale("en"));
 
   // 确保存在本地默认用户供本地使用
   await UserService.ensureLocalUserExists();
 
-  runApp(const MyApp());
+  runApp(Phoenix(
+    child: MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ThemeNotifier()),
+        ChangeNotifierProvider(create: (_) => LanguageNotifier()), // 新增
+      ],
+      child: const MyApp(),
+    ),
+  ),);
 }
 
 // 将async添加到函数中,即便没有显式的返回一个Future对象
@@ -30,17 +42,31 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    final languageNotifier = Provider.of<LanguageNotifier>(context);
+
     return MaterialApp(
       title: 'Assistant Tracker',
-      home: //BillHomePage(),
-          BillHomePage(),
-      /*builder: (context, child) {
-        return MediaQuery(
-          //// 全局取消因键盘引起的挤压
-          data: MediaQuery.of(context).copyWith(viewInsets: EdgeInsets.zero),
-          child: child!,
-        );
-      },*/
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeNotifier.themeMode,
+      locale: languageNotifier.locale,
+      supportedLocales: const [
+        Locale('en'),
+        Locale('zh'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        MonthYearPickerLocalizations.delegate,
+      ],
+      home: BillHomePage(),
+      routes: {
+        "/category": (context) => CategoryManagerPage(),
+        "/language": (context) => const LanguageSettingPage(),
+        "/settings": (context) => const SettingPage(),
+      },
     );
   }
 }
