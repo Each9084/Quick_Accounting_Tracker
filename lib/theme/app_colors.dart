b88eb8b880de34c157a0822a01b0d808b56691a7
Each9084 <each9084@gmail.com>
2025-05-26 12:56:30 +0100

    add color

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // 浅色主题
+  static const lightBackground = Color(0xFFF8F9FA);
+  static final lightPrimary = Colors.indigo.shade900;
+  static const lightText = Colors.black87;
+
+  // 深色主题
+  static const darkBackground = Color(0xFF121212);
+  static const darkPrimary = Color(0xFF303F9F);
+  static const darkText = Colors.white;
+}
